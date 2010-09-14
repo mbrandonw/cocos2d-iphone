73d258925ca81815d4be58eea491c832869e598a
Brandon Williams <brandon@opetopic.com>
2010-09-14 18:29:22 -0400

    Fixed another bug with extinguished particles sticking on the screen.

--- cocos2d/CCParticleSystem.m
+++ cocos2d/CCParticleSystem.m
@@ -441,7 +441,7 @@
 		} else if (reuseParticles_) {
 			
 			// reset the particle right after it extinguishes
-			if (p->timeToLive >= life-lifeVar) {
+			if ((p->timeToLive >= -2.0f * dt) || (p->timeToLive >= life-lifeVar)) {
 				
 				// move dead particles off the screen
 				p->pos.x = -9999.0f;
